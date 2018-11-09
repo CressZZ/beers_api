42567ad17c5292b63af4d82400c18c93fbae3573
CressZZ <dktsjssnrnsi@gmail.com>
2018-11-10 03:02:15 +0900

    맥주 리스트 불러오기 기능 마무리
    - 태그는 토글 할 수 있으며 선택한 태그 조건에 따라 아래 맥주 목록이 갱신됨
    - 목록은 일치하는 태그가 많은 순으로 정렬함

--- readme.md
+++ readme.md
@@ -1,4 +1,22 @@
 # API server
 
+## 조건에서 제거하고, 추가한 부분
+### 맥주목록 가져오기
+```js
+[{
+    id: number, 
+    name: string, 
+    image: string, 
+    tags: [{
+        // key: string, // 제거. 사용하는 곳 없음
+        name: string 
+    },
+    price: number,
+    stock: number, 
+ 
+    }
+    , ...]
+```
+
 ## note
 - config/database.js 는 서버와 로컬환경이 각각 다르기 때문에 `.gitignore` 에 추가함
--- routes/beers.js
+++ routes/beers.js
@@ -8,26 +8,50 @@ router.get('/list/:tag', getBeers);
  * params로 전달된 tag에 따라 맥주 리스트를 불러온다. 
  * 목록은 일치하는 태그가 많은 순으로 정렬함
  * @param {string} req.params.tag // ex) 'A_BG_C_BK'
+ * @return {array-json} 맥주리스트 객체
  */
 async function getBeers(req, res, next) {
     let tagsS = req.params.tag; 
-    tagsS = tagsS.replace(/_/g, ','); 
+    tagsS = tagsS.replace(/_/g, '\",\"'); 
 
-    var sql = `
+    // parms(tag) 값에 따라 쿼리 설정
+    let sql = `
         select 
-            B.*, GROUP_CONCAT(TLB.tags_key) as tags, count(TLB.tags_key) as cnt 
+            B.id, B.name, B.image, B.price, B.stock,
+            GROUP_CONCAT(T.name) as tags 
         from 
-            beers B join tags_link_beers TLB on B.id = TLB.beers_id 
+            beers B 
+            join tags_link_beers TLB on B.id = TLB.beers_id
+            join tags T on TLB.tags_key = T.key
         where 
-            TLB.tags_key in (${tagsS}) 
+            TLB.tags_key in (\"${tagsS}\") 
         group by 
             B.id 
         order by 
-            cnt DESC;
+            count(TLB.tags_key) DESC;
     `;
-    var result = await dao.query(sql);
-    
-    res.json(result);
+
+    // 쿼리를 날린다
+    let beers = await dao.query(sql);
+
+    // DB에서 받아온 맥주리스트 전처리
+    beers = preProcessBeers(beers);
+
+    res.json(beers);
 }
+
+/**
+ * @param {array} 맥주리스트 객체
+ * tags에 대한 값으로 들어온 String 타입을 
+ * 과제 조건에 맞게 Array로 변경
+ */
+function preProcessBeers(beers){
+    let _beers = beers.map((beer)=>{
+        beer.tags = beer.tags.split(',')
+        return beer
+    })
+    return _beers;
+}
+
 
 module.exports = router;
