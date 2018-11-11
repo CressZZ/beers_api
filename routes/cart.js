c4f76055f7bd9a074b2a0d6855e53a2c8a68b2fa
CressZZ <dktsjssnrnsi@gmail.com>
2018-11-11 10:29:53 +0900

    장바구니 API 전면 수정 - '추가/삭제' 와 '수량변경' 으로 나뉘어 있던 API를 하나로 통일 - 내부로직에서  '추가/삭제' 와 '수량변경' 인지 확인 함

--- routes/cart.js
+++ routes/cart.js
@@ -3,8 +3,7 @@ var router = express.Router();
 var dao = require('../common_dao');
 
 
-router.get('/cnt/:action/:userId/:beerId/:cnt',cartCnt ); 
-router.get('/:action/:userId/:beerId/',cartAction ); 
+router.get('/:action/:userId/:beerId/:cnt',cartCnt ); 
 router.get('/:userId',getCart ); 
 
 /**
@@ -20,49 +19,6 @@ async function getCart(req, res, next) {
     let cart =  await dao.query(sql_cart);
 
     res.json({result:"장바구니 조회 성공", status:200, cart:cart});
-
-}
-/**
- * 장바구니에 아이탬 추가 / 삭제
- * @param {string} action 'add' / 'del'
- * @param {number} userId 유저 아이디
- * @param {number} beerId 맥주 아이디
- * @param {object} {result:"장바구니 추가 성공", status:200, cart:cart}
- */
-async function cartAction(req, res, next) {
-    let action = req.params.action;
-    let beerId = req.params.beerId;
-    let userId = req.params.userId;
-
-    let sql_cartActionCheck = `select user_id, beer_id, count from cart where user_id = ${userId} and beer_id = ${beerId}`;
-    let cartActionCheck = await dao.query(sql_cartActionCheck);
-    let isExistBeer = cartActionCheck.length > 0 ? true : false;
-
-    // 장바구니 액션 쿼리
-    let sql_cartAction
-    if(action == "add"){
-        if(isExistBeer){
-            res.json({result:"장바구니 실패", status:500});
-            return false
-        }
-        sql_cartAction = `INSERT INTO cart  (user_id, beer_id, count) VALUES (${userId}, ${beerId}, 1);`
-    }else if (action == "del"){
-        if(!isExistBeer){
-            res.json({result:"장바구니 실패", status:500});
-            return false
-        }
-        sql_cartAction = `DELETE FROM cart where user_id = ${userId} and beer_id = ${beerId};`
-    }else{
-        return false; 
-    }
-
-    await dao.query(sql_cartAction);
-
-    // 장바구니 현황 쿼리
-    let sql_cart = `select user_id, beer_id, count from cart where user_id = ${userId}`
-    let cart =  await dao.query(sql_cart);
-
-    res.json({result:"장바구니 추가/삭제 성공", status:200, cart:cart});
 
 }
 
@@ -81,42 +37,83 @@ async function cartCnt(req, res, next) {
     let cnt = Number(req.params.cnt);
     
     // 1. 일단 DB에 있는 상품의 재고파악
-    let sql_stock = `select stock from beers where id = ${beerId};`
-    let nowStock = await dao.query(sql_stock);
-    nowStock = Number(nowStock[0].stock);
-    // 장바구니 담기 액션이고, 재고가 모자랄때
+    let nowStock = await getNowStoc(beerId);
+
+    // 2. 유저 장바구니에 있는 해당 아이템 수량 파악
+    let nowCount = await getNowCount(userId, beerId);
+
+    // 3. 장바구니 담기 액션이고, 재고가 모자랄때 리턴 펄스
     if(action == 'plus' && nowStock < cnt){
         res.json({result:`재고가 모자랍니다.`, status:500});
         return false;
     }
 
-    // 2. DB 재고 업데이트 - DB 업데이트 할 재고숫자
+    // 4. 장바구니 빼기 액션인데 유저에게 상품이 없을때 리터 펄스 
+    if(action == 'minus' && nowCount < 1){
+        res.json({result:`장바구니에 해당 상품이 없습니다. .`, status:500});
+        return false;
+    }
+
+    // 5. 업데이트 가능 조건일때 - DB 재고 업데이트 
+        // action 값에 따라 재고 목표 숫자 설정
     let targetStock = action == 'plus' ? (nowStock - cnt) : (nowStock + cnt)  
-        // parms(tag) 값에 따라 쿼리 설정
-    let sql_removeStock = `
-        update beers set stock = ${targetStock} where id = ${beerId}; 
-    `;
-        // 쿼리를 날린다
+    let sql_removeStock = `update beers set stock = ${targetStock} where id = ${beerId}; `;
     await dao.query(sql_removeStock);
 
-    // 3. 장바구니 업데이트 -  유저 장바구니 내역
-    let sql_count = `select count from cart where user_id = ${userId} and beer_id = ${beerId};`
-    let nowCount = await dao.query(sql_count);
-    nowCount = Number(nowCount[0].count);
-    let targetCnt = action == 'plus' ? (nowCount + cnt) : (nowCount - cnt)  
+    // 6. 업데이트 가능 조건일때 - 유저 장바구니 업데이트
+        // action 값에 따라 장바구니 해당 아이탬 목표 수량 설정
+    let sql_updateCart = setUpdateCartSql(action, nowCount, cnt, userId, beerId);
+    await dao.query(sql_updateCart);
+    
 
-    let sql_nowCart = `
-        update cart set count = ${targetCnt}  where user_id = ${userId} and beer_id = ${beerId}; 
-    `;
-
-    await dao.query(sql_nowCart);
-
-
-    // 4.장바구니 현황 쿼리
+    // 7. 장바구니 현황 파악
     let sql_cart = `select user_id, beer_id, count from cart where user_id = ${userId}`
     let cart =  await dao.query(sql_cart);
 
     res.json({result:"수량변경 성공", status:200, cart:cart});
 }
 
+/**
+ * DB에 있는 상품의 재고파악
+ * @param {number} beerId 
+ */
+async function getNowStoc(beerId){
+    let sql_stock = `select stock from beers where id = ${beerId};`
+    let nowStock = await dao.query(sql_stock);
+    nowStock = Number(nowStock[0].stock);
+
+    return nowStock
+}
+
+/**
+ * 유저 장바구니에 있는 해당 아이템 수량 파악
+ * @param {number} userId 
+ * @param {number} beerId 
+ */
+async function getNowCount(userId, beerId){
+    let sql_count = `select count from cart where user_id = ${userId} and beer_id = ${beerId};`
+    let nowCount = await dao.query(sql_count);
+    nowCount = nowCount[0] ? Number(nowCount[0].count) : 0;
+    
+    return nowCount
+}
+
+/**
+ * action 값에 따라 장바구니 해당 아이탬 목표 수량 설정
+ */
+function setUpdateCartSql(action, nowCount, cnt, userId, beerId){
+    let _sql_updateCart;
+    let targetCnt = action == 'plus' ? (nowCount + cnt) : (nowCount - cnt)  
+    if(targetCnt == 0){
+        // 신규추가 일때
+        _sql_updateCart = `DELETE FROM cart where user_id = ${userId} and beer_id = ${beerId};`
+    }else if (targetCnt == 1){
+        // 삭제 일때
+        _sql_updateCart = `INSERT INTO cart  (user_id, beer_id, count) VALUES (${userId}, ${beerId}, 1);`
+    }else{
+        // 수량 변경일 때 
+        _sql_updateCart = `update cart set count = ${targetCnt}  where user_id = ${userId} and beer_id = ${beerId}; `;
+    }
+    return _sql_updateCart
+}
 module.exports = router;
